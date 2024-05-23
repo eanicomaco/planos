f3d4a5051dae87dd581a81aadc9e2fc9526aa0dc
eanicomaco <eanicomaco@gmail.com>
2024-05-23 15:52:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// var screenWidth = window.innerWidth;
+// var containerWidth = screenWidth * 0.9;
+// var divWidth = (containerWidth - 30) / 3; // Assume 15px spacing
+
+// if (screenWidth >= 768) { // Assume 768px as the breakpoint for mobile devices
+//     var divs = document.querySelectorAll('.cardPlanos');
+//     divs.forEach(function(div) {
+//         div.style.width = divWidth + 'px';
+//     });
+// }
